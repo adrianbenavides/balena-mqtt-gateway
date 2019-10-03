e06a66539b6b4f3e436f10472e72202887cd3ef1
Adrian Benavides <adri.benavides312@gmail.com>
2019-10-03 18:20:38 +0200

    chore: add another map plugin to test different layouts

--- grafana/entry.sh
+++ grafana/entry.sh
@@ -3,6 +3,7 @@
 /usr/src/app/api.sh
 
 grafana-cli plugins install alexandra-trackmap-panel
+grafana-cli plugins install pr0ps-trackmap-panel
 
 grafana-server                                              \
     --homepath /usr/share/grafana                           \
